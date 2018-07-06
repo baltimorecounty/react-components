bad1d8cebd71b4c2b443855717caa93ba826c6cf
Marty Powell <jpowel25@gmail.com>
2018-07-06 13:51:01 -0400

    split out examples into html and react

--- src/docs/ComponentPage.js
+++ src/docs/ComponentPage.js
@@ -5,24 +5,32 @@ import Props from './Props';
 
 const ComponentPage = ({component}) => {
   const {name, description, props, examples} = component;
+  const reactExamples = examples.filter(example => example.fileType === 'js');
+  const htmlExamples = examples.filter(example => example.fileType === 'html');
 
   return (
     <div className="componentpage">
       <h2>{name}</h2>
 	  <div dangerouslySetInnerHTML={{ __html: description }}/>
 
-      <h3>Example{examples.length > 1 && "s"}</h3>
+      <h3>React Example{reactExamples.length > 1 && "s"}</h3>
       {
-        examples.length > 0 ?
-        examples.map( example => <Example key={example.name} example={example} componentName={name} /> ) :
-        "No examples exist."
-      }
-
-      <h3>Props</h3>
+        reactExamples.length > 0 ?
+        reactExamples.map( example => <Example key={example.name} example={example} componentName={name} /> ) :
+        "No React examples exist."
+	  }
+      <h4>Props</h4>
       {
         props ?
         <Props props={props} /> :
         "This component accepts no props."
+	  }
+
+	  <h3>Html Example{htmlExamples.length > 1 && "s"}</h3>
+      {
+        htmlExamples.length > 0 ?
+        htmlExamples.map( example => <Example key={example.name} example={example} componentName={name} /> ) :
+        "No Html examples exist."
       }
     </div>
   )
