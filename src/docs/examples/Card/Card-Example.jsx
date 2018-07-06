812b74959c68d7dd9c12108d54f8feae23650ccb
Marty Powell <jpowel25@gmail.com>
2018-07-06 10:08:02 -0400

    added samples

---
+++ src/docs/examples/Card/Card-Example.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Card from 'bc-components/Card';
+
+export default () => (
+    <Card>
+        <h2>Example Card</h2>
+        <p>This is a sample card</p>
+    </Card>
+);
---
+++ src/docs/examples/Loader/Loader-WithMessage-Example.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import Loader from 'bc-components/Loader';
+
+export default () => <Loader message="Some other loading text..." />;
