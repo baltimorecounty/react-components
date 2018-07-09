5926213e70ed93e6633ea19736532f8458dae565
Marty Powell <jpowel25@gmail.com>
2018-07-09 10:28:41 -0400

    added navigation to the page

--- src/docs/Navigation.js
+++ src/docs/Navigation.js
@@ -1,24 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Menu, Icon } from 'antd';
 
-const Navigation = ({components}) => {
-  return (
-    <ul className="navigation">
-      {
-        components.map( name => {
-          return (
-            <li key={name}>
-              <a href={`#${name}`}>{name}</a>
-            </li>
-          )
-        })
-      }
-    </ul>
-  )
+const SubMenu = Menu.SubMenu;
+
+class Navigation extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            openKeys: [],
+            rootSubmenuKeys: this.props.components.slice()
+        };
+
+        console.log(this.props.components);
+    }
+
+    onOpenChange = openKeys => {
+        const latestOpenKey = openKeys.find(
+            key => this.state.openKeys.indexOf(key) === -1
+        );
+        if (this.state.rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+            this.setState({ openKeys });
+        } else {
+            this.setState({
+                openKeys: latestOpenKey ? [latestOpenKey] : []
+            });
+        }
+    };
+
+    render() {
+        const { components } = this.props;
+        return (
+            <Menu
+                mode="inline"
+                openKeys={this.state.openKeys}
+                onOpenChange={this.onOpenChange}
+                style={{ width: 256 }}
+            >
+                {components.map((name, index) => {
+                    return (
+                        <SubMenu key={`${name}-sub-${index}`} title={name}>
+                            <Menu.Item key={`${name}-1`}>
+                                <a href={`#${name}`}>{name}</a>
+                            </Menu.Item>
+                        </SubMenu>
+                    );
+                })}
+            </Menu>
+        );
+    }
 }
 
 Navigation.propTypes = {
-  components: PropTypes.array.isRequired
+    components: PropTypes.array.isRequired
 };
 
 export default Navigation;
