619a6032d92cb0cb76ecdeb1f8ed244b7e8970ad
Marty Powell <jpowel25@gmail.com>
2018-07-09 14:20:40 -0400

    adjusted logic to group examples

--- src/docs/ComponentPage.js
+++ src/docs/ComponentPage.js
@@ -1,43 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Example from './Example';
-import Props from './Props';
+import { getUniqueObjectsByKey } from '../utilities/helpers';
 
-const ComponentPage = ({component}) => {
-  const {name, description, props, examples} = component;
-  const reactExamples = examples.filter(example => example.fileType === 'js');
-  const htmlExamples = examples.filter(example => example.fileType === 'html');
+const ComponentPage = ({ component }) => {
+	const { name, description, props, examples } = component;
+	const uniqueExamples = getUniqueObjectsByKey(examples, 'name');
+	const numberOfUniqueExamples = Object.keys(uniqueExamples).length;
 
-  return (
-    <div className="componentpage">
-      <h2>{name}</h2>
-	  <div dangerouslySetInnerHTML={{ __html: description }}/>
-
-      <h3>React Example{reactExamples.length > 1 && "s"}</h3>
-      {
-        reactExamples.length > 0 ?
-        reactExamples.map( example => <Example key={example.name} example={example} componentName={name} /> ) :
-        "No React examples exist."
-	  }
-      <h4>Props</h4>
-      {
-        props ?
-        <Props props={props} /> :
-        "This component accepts no props."
-	  }
-
-	  <h3>Html Example{htmlExamples.length > 1 && "s"}</h3>
-      {
-        htmlExamples.length > 0 ?
-        htmlExamples.map( example => <Example key={example.name} example={example} componentName={name} /> ) :
-        "No Html examples exist."
-      }
-    </div>
-  )
+    return (
+        <div className="componentpage">
+            <h2>{name}</h2>
+            <div dangerouslySetInnerHTML={{ __html: description }} />.
+            <h3>Example{numberOfUniqueExamples > 1 && 's'}</h3>
+            {numberOfUniqueExamples > 0
+                ? Object.keys(uniqueExamples).map((key, index) => {
+					  const examples = uniqueExamples[key];
+                      return <Example
+                          key={index}
+                          examples={examples}
+                          componentName={name}
+                          componentProps={props}
+                      />
+				})
+                : 'No React examples exist.'}
+        </div>
+    );
 };
 
 ComponentPage.propTypes = {
-  component: PropTypes.object.isRequired
+    component: PropTypes.object.isRequired
 };
 
 export default ComponentPage;
